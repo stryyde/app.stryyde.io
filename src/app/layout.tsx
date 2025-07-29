0fabb7ce4c33ec61b04faa6694b73e94eef02fd1
sam4llis <samwilliamellis@gmail.com>
2025-07-29 21:42:58 +0100

    chore: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "stryyde",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
