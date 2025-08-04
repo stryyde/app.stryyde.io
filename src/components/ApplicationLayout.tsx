e2c7db972893bbb2dba05ec1feeb1a341b9e0139
sam4llis <samwilliamellis@gmail.com>
2025-08-04 09:17:11 +0200

    feat: add header to application layout

--- src/components/ApplicationLayout.tsx
+++ src/components/ApplicationLayout.tsx
@@ -1,10 +1,84 @@
+import {
+  ArrowRightStartOnRectangleIcon,
+  BellIcon,
+  ChatBubbleLeftRightIcon,
+  CogIcon,
+  UserIcon,
+} from "@heroicons/react/16/solid";
+
+import { LogoIcon } from "@/components/icons/LogoIcon";
 import { ThemeToggle } from "@/components/ThemeToggle";
+import { Avatar } from "@/components/ui/Avatar";
+import {
+  Dropdown,
+  DropdownButton,
+  DropdownDivider,
+  DropdownItem,
+  DropdownLabel,
+  DropdownMenu,
+} from "@/components/ui/Dropdown";
+import { Link } from "@/components/ui/Link";
+import {
+  Navbar,
+  NavbarDivider,
+  NavbarItem,
+  NavbarSection,
+  NavbarSpacer,
+} from "@/components/ui/Navbar";
+import { Sidebar } from "@/components/ui/Sidebar";
+import { StackedLayout } from "@/components/ui/StackedLayout";
 
 export function ApplicationLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main>
-      <ThemeToggle />
+    <StackedLayout
+      navbar={
+        <Navbar>
+          <Link href="/">
+            <LogoIcon className="mx-2.5 h-6 w-6" />
+          </Link>
+          <NavbarDivider className="max-lg:hidden" />
+          <NavbarSection className="max-lg:hidden">
+            <NavbarItem href="/exercises">Exercises</NavbarItem>
+          </NavbarSection>
+          <NavbarSpacer />
+          <NavbarSection>
+            <Dropdown>
+              <DropdownButton plain aria-label="Notifications">
+                <BellIcon />
+              </DropdownButton>
+            </Dropdown>
+            <ThemeToggle />
+            <Dropdown>
+              <DropdownButton as={NavbarItem} aria-label="Account Menu">
+                <Avatar initials="ST" />
+              </DropdownButton>
+              <DropdownMenu className="min-w-64" anchor="bottom end">
+                <DropdownItem href="/me/profile">
+                  <UserIcon />
+                  <DropdownLabel>Profile</DropdownLabel>
+                </DropdownItem>
+                <DropdownItem href="/me/settings">
+                  <CogIcon />
+                  <DropdownLabel>Settings</DropdownLabel>
+                </DropdownItem>
+                <DropdownDivider />
+                <DropdownItem>
+                  <ChatBubbleLeftRightIcon />
+                  <DropdownLabel>Feedback</DropdownLabel>
+                </DropdownItem>
+                <DropdownDivider />
+                <DropdownItem>
+                  <ArrowRightStartOnRectangleIcon />
+                  <DropdownLabel>Sign out</DropdownLabel>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarSection>
+        </Navbar>
+      }
+      sidebar={<Sidebar>TODO</Sidebar>}
+    >
       {children}
-    </main>
+    </StackedLayout>
   );
 }
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -4,6 +4,7 @@ import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 import { ThemeIcon } from "@/components/icons/ThemeIcon";
+import { NavbarItem } from "@/components/ui/Navbar";
 
 export function ThemeToggle() {
   const [mounted, setMounted] = useState(false);
@@ -19,13 +20,13 @@ export function ThemeToggle() {
   }
 
   return (
-    <button
+    <NavbarItem
       aria-label={mounted ? `Switch to ${otherTheme} theme` : "Toggle theme"}
-      className="group z-50 -m-2.5 p-2.5"
+      className="group z-50"
       onClick={() => setTheme(otherTheme)}
     >
       <span className="sr-only">Switch to {otherTheme} theme</span>
-      <ThemeIcon className="h-6 w-6 fill-white opacity-50 transition-opacity group-hover:opacity-100 lg:fill-zinc-900 lg:dark:fill-white" />
-    </button>
+      <ThemeIcon className="-m-0.5 h-6 w-6 fill-white opacity-50 transition-opacity group-hover:opacity-100 lg:fill-zinc-900 lg:dark:fill-white" />
+    </NavbarItem>
   );
 }
